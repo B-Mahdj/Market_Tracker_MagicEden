089c7a10382c965d96115271d0e40e9cda738283
B-Mahdj <bilal.mahdjoubi95@gmail.com>
2022-03-19 19:43:24 +0100

    Starting tracker

--- src/tracker.js
+++ src/tracker.js
@@ -1,9 +1,7 @@
 //Main entrypoint of our bot
 //Call environment variables inside the .env file
 require('dotenv').config();
-MILLISECONDES_BEFORE_REFRESH=15000;
-NUMBER_FOR_DIVIDE=1000000000;
-NUMBER_OF_NFT_IN_COLLECTION=10000;
+MILLISECONDES_BEFORE_REFRESH=1000;
 
 //print dans la console la variable nommée dans .env 
 //console.log(process.env.DISCORD_BOT_TOKEN);
@@ -19,7 +17,6 @@ client.on('ready', async () => {
   const guild = await client.guilds.fetch(GUILD_ID[0]);
   const https = require('https');
   console.log('Bot is connected...');
-  var floor_price,avgPrice24h, listedCount, volumeAll, percentage_listed;
 
   setInterval(async function(){
     // Call the magicEden API to get details on the angrybearclub collection (var for the name)
@@ -35,35 +32,32 @@ client.on('ready', async () => {
       var json = JSON.parse(body);
       console.log(json);
       //Get Floor price of the collection
-      floor_price=json.floorPrice / NUMBER_FOR_DIVIDE;
-      avgPrice24h=floatParse2(json.avgPrice24hr / NUMBER_FOR_DIVIDE);
-      listedCount=json.listedCount;
-      volumeAll=floatParse2(json.volumeAll / NUMBER_FOR_DIVIDE);
-      percentage_listed= floatParse2((listedCount / NUMBER_OF_NFT_IN_COLLECTION) * 100);
-
-      //Print it in console 
-      console.log("The floor price returned is : ")
-      console.log(floor_price);
-      console.log("The avg price on 24h returned is : ")
-      console.log(avgPrice24h);
-      console.log("The number of listed NFT is : ")
-      console.log(listedCount);
-      console.log("The volume for All time is : ")
-      console.log(volumeAll);
     });
-
-    //V2 : Print the floor price in profile description 
-    const nickname = `FP\: ${floor_price} SOL`;
-    console.log(nickname);
-    console.log(`Avg 24h : ${avgPrice24h} SOL`);
-    guild.me.setNickname(nickname);
-    client.user.setActivity(`Avg 24h: ${avgPrice24h} SOL/${listedCount} listed (${percentage_listed}%)/ VolumeALL : ${volumeAll}`, { type: 'WATCHING' });
   }, MILLISECONDES_BEFORE_REFRESH)
 
 });
 
 function floatParse2(x) {
   return Number.parseFloat(x).toFixed(2);
+}
+
+function getWalletActivities(){
+    setInterval(async function(){
+        // Call the magicEden API to get details on the angrybearclub collection (var for the name)
+        var request = require('request');
+        var options = {
+          'method': 'GET',
+          'url': 'api-mainnet.magiceden.dev/v2/wallets/:wallet_address/activities?offset=0&limit=100',
+          'headers': {
+          }
+        };
+        request(options, function (error, response, body) {
+          if(error) console.log('error', err);
+          var json = JSON.parse(body);
+          console.log(json);
+          //Get Floor price of the collection
+        });
+      }, MILLISECONDES_BEFORE_REFRESH)
 }
 
 //Log our bot in using the variable 
